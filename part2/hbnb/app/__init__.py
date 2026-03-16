c7622090a44319e4d2c552afeb0589e10ea4d44e
SabyJeany <sabyjeany@gmail.com>
2026-03-16 01:47:32 +0100

    init app: configure flask-restx and api routes initialization

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restx import Api
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     """Factory function to create and configure the Flask application."""
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     from app.api.v1.users import api as users_ns
@@ -15,5 +16,4 @@ def create_app():
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
-    
     return app
