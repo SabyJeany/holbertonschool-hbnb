3976db0f40db8c5aa5500737e895d861f5566c1d
SabyJeany <sabyjeany@gmail.com>
2026-04-02 12:30:18 +0200

    Initialize core extensions: Bcrypt, JWT, SQLAlchemy, and CORS.

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
@@ -15,6 +16,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
+    CORS(app)
 
     api = Api(
         app, 
